896b45e5b2b30e6a28939623c5cb72cf45d4b5ca
shosprojects <5h0pp25267@gmail.com>
2025-01-27 22:25:59 +0100

    a wild comment appears!

--- src/main/java/pl/sho/slm/ShosLearningMod.java
+++ src/main/java/pl/sho/slm/ShosLearningMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//comment
 public class ShosLearningMod implements ModInitializer {
 	public static final String MOD_ID = "shoslearningmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
